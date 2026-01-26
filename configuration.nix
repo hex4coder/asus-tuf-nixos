d5e28c24441c3c726295a341ca58d5ceeaf40b12
hex4coder <the.programmer.luyo@gmail.com>
2026-01-27 04:59:39 +0800

    Disable the security wrapper for ubridge

--- configuration.nix
+++ configuration.nix
@@ -306,14 +306,14 @@
 	vpcs.enable = true;
 	dynamips.enable = true;
   };
-  security.wrappers.ubridge = {
-    source = "${pkgs.ubridge}/bin/ubridge";
-    capabilities = "cap_net_admin,cap_net_raw=ep";
-    owner = "root";
-    group = "ubridge";
-    permissions = "u+rx,g+x";
-  };
-
+  # security.wrappers.ubridge = {
+  #   source = "${pkgs.ubridge}/bin/ubridge";
+  #   capabilities = "cap_net_admin,cap_net_raw=ep";
+  #   owner = "root";
+  #   group = "ubridge";
+  #   permissions = "u+rx,g+x";
+  # };
+  #
   programs.dconf.enable = true;
 
   # asus
