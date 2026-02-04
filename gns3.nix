f07a7bf100fec86254d1f7eadb9e261c2753e7f5
hex4coder <the.programmer.luyo@gmail.com>
2026-02-05 06:55:06 +0800

    Update fix gns3 fix runtime 2

--- gns3.nix
+++ gns3.nix
@@ -3,8 +3,8 @@
 let
   autoVersion = src: "3.0.6-unstable-${src.lastModifiedDate or "latest"}";
   
-  # Satukan semua dependensi Python agar versinya sinkron
-  pythonWithDeps = pkgs.python3.withPackages (ps: with ps; [
+  # List paket individual agar mudah dikelola
+  sharedPythonPkgs = with pkgs.python3Packages; [
     sip
     pyqt5
     setuptools
@@ -21,7 +21,10 @@ let
     async-timeout
     py-cpuinfo
     platformdirs
-  ]);
+  ];
+
+  # Helper untuk PYTHONPATH
+  pythonEnv = pkgs.python3.withPackages (ps: sharedPythonPkgs);
 
 in {
   users.users.kaco.packages = [
@@ -30,18 +33,17 @@ in {
       version = autoVersion gns3-gui-src;
       src = gns3-gui-src;
       
-      # Gunakan nativeBuildInputs untuk menyertakan makeWrapper
+      # Masukkan individual agar pythonRuntimeDepsCheck senang
+      propagatedBuildInputs = sharedPythonPkgs;
+
       nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ 
         pkgs.makeWrapper 
         pkgs.qt5.wrapQtAppsHook 
       ];
 
-      propagatedBuildInputs = [ pythonWithDeps ];
-
-      # Paksa PYTHONPATH agar menunjuk ke library yang benar saat dijalankan
       postInstall = ''
         wrapProgram $out/bin/gns3 \
-          --prefix PYTHONPATH : "${pythonWithDeps}/${pkgs.python3.sitePackages}"
+          --prefix PYTHONPATH : "${pythonEnv}/${pkgs.python3.sitePackages}"
       '';
 
       doCheck = false;
@@ -54,12 +56,13 @@ in {
       version = autoVersion gns3-server-src;
       src = gns3-server-src;
       
+      propagatedBuildInputs = sharedPythonPkgs;
+
       nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-      propagatedBuildInputs = [ pythonWithDeps ];
 
       postInstall = ''
         wrapProgram $out/bin/gns3server \
-          --prefix PYTHONPATH : "${pythonWithDeps}/${pkgs.python3.sitePackages}"
+          --prefix PYTHONPATH : "${pythonEnv}/${pkgs.python3.sitePackages}"
       '';
 
       doCheck = false;
@@ -74,7 +77,7 @@ in {
     pkgs.xterm 
   ];
 
-  # Keamanan & Grup tetap sama
+  # Tetap pertahankan konfigurasi security
   security.wrappers.ubridge = {
     source = "${pkgs.ubridge}/bin/ubridge";
     capabilities = "cap_net_admin,cap_net_raw+ep";
