309c291d240d27e562eddef124e4ef4175a23497
hex4coder <the.programmer.luyo@gmail.com>
2026-02-05 06:49:27 +0800

    Update fix gns3 7

--- gns3.nix
+++ gns3.nix
@@ -3,6 +3,7 @@
 let
   autoVersion = src: "3.0.6-unstable-${src.lastModifiedDate or "latest"}";
   
+  # Daftar dependensi lengkap untuk GNS3 v3
   gns3Deps = with pkgs.python3Packages; [
     sip
     pyqt5
@@ -10,7 +11,9 @@ let
     psutil
     jsonschema
     distutils
-    raven
+    sentry-sdk    # Baru ditambahkan
+    truststore    # Baru ditambahkan
+    resource
     distro
     setuptools-scm
   ];
@@ -18,50 +21,51 @@ let
 in {
   users.users.kaco = {
     packages = [
-      # GNS3 GUI dengan Override
       (pkgs.gns3-gui.overrideAttrs (old: {
         version = autoVersion gns3-gui-src;
         src = gns3-gui-src;
+
         propagatedBuildInputs = gns3Deps;
+
+        # Bypass checks agar build tidak berhenti karena masalah lingkungan sandbox
         doCheck = false;
         doInstallCheck = false;
         dontUsePytestCheck = true;
         pythonImportsCheck = [ ];
+
         nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ 
           pkgs.qt5.wrapQtAppsHook 
         ];
       }))
 
-      # GNS3 Server dengan Override
       (pkgs.gns3-server.overrideAttrs (old: {
         version = autoVersion gns3-server-src;
         src = gns3-server-src;
+        
         doCheck = false;
         doInstallCheck = false;
         dontUsePytestCheck = true;
         pythonImportsCheck = [ ];
+
         propagatedBuildInputs = (with pkgs.python3Packages; [
           setuptools
           aiohttp
           jsonschema
           psutil
+          sentry-sdk # Server juga butuh sentry biasanya
         ]);
       }))
 
-      # Emulator & Tools
       pkgs.ubridge
       pkgs.dynamips
       pkgs.vpcs
       pkgs.wireshark
-      
-      # Terminal Emulator untuk Konsol GNS3
       pkgs.xterm 
     ];
     
     extraGroups = [ "gns3" "ubridge" "wireshark" ];
   };
 
-  # Keamanan ubridge
   security.wrappers.ubridge = {
     source = "${pkgs.ubridge}/bin/ubridge";
     capabilities = "cap_net_admin,cap_net_raw+ep";
@@ -70,7 +74,6 @@ in {
     permissions = "u+rx,g+x,o=";
   };
 
-  # Definisi Grup
   users.groups.gns3 = {};
   users.groups.ubridge = {};
 }
