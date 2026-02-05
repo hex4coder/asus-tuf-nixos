1accf6caad9f8610394b448a871e110642ff1730
hex4coder <the.programmer.luyo@gmail.com>
2026-02-05 08:29:14 +0800

    Update fix gns3 fix runtime 22

--- gns3.nix
+++ gns3.nix
@@ -1,77 +1,12 @@
-{ pkgs, gns3-gui-src, gns3-server-src, ... }:
+{ pkgs, ... }:
 
 let
   autoVersion = src: "3.0.6-unstable-${src.lastModifiedDate or "latest"}";
   
-  sharedPythonPkgs = with pkgs.python3Packages; [
-    pyqt6
-    pyqt6-sip
-    setuptools
-    psutil
-    jsonschema
-    distutils
-    sentry-sdk
-    truststore
-    distro
-    setuptools-scm
-    aiohttp
-    aiofiles
-    jinja2
-    async-timeout
-    py-cpuinfo
-    platformdirs
-  ];
-
-  pythonEnv = pkgs.python3.withPackages (ps: sharedPythonPkgs);
-
 in {
-  users.users.kaco.packages = let
-    myGns3Gui = pkgs.gns3-gui.overrideAttrs (old: {
-      version = autoVersion gns3-gui-src;
-      src = gns3-gui-src;
-      propagatedBuildInputs = sharedPythonPkgs;
-
-      postPatch = ''
-        # Hapus blok dictionary yang bermasalah (dari kurung buka sampai kurung tutup)
-        sed -i '/PRECONFIGURED_VNC_CONSOLE_COMMANDS = {/,/}/d' gns3/settings.py
-        # Masukkan kembali variabel kosong dengan indentasi 4 spasi yang benar
-        # Kita masukkan di baris 181 agar berada di bawah pernyataan 'if' yang menunggunya
-        sed -i '181i\    PRECONFIGURED_VNC_CONSOLE_COMMANDS = {}' gns3/settings.py
-      '';
-
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ 
-        pkgs.makeWrapper 
-        pkgs.qt6.wrapQtAppsHook 
-      ];
-
-      postFixup = ''
-        wrapProgram $out/bin/gns3 \
-          --set PYTHONPATH "$out/${pkgs.python3.sitePackages}:${pythonEnv}/${pkgs.python3.sitePackages}"
-      '';
-
-      doCheck = false;
-      dontUsePytestCheck = true;
-      pythonImportsCheck = [ ];
-    });
-
-    myGns3Server = pkgs.gns3-server.overrideAttrs (old: {
-      version = autoVersion gns3-server-src;
-      src = gns3-server-src;
-      propagatedBuildInputs = sharedPythonPkgs;
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-
-      postFixup = ''
-        wrapProgram $out/bin/gns3server \
-          --set PYTHONPATH "$out/${pkgs.python3.sitePackages}:${pythonEnv}/${pkgs.python3.sitePackages}"
-      '';
-
-      doCheck = false;
-      dontUsePytestCheck = true;
-      pythonImportsCheck = [ ];
-    });
-  in [
-    myGns3Gui
-    myGns3Server
+  users.users.kaco.packages = [ 
+    pkgs.gns3gui
+    pkgs.gns3server
     pkgs.ubridge
     pkgs.dynamips
     pkgs.vpcs
