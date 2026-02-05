af0613ece4cd145a580bdb14af673f43cbc20874
hex4coder <the.programmer.luyo@gmail.com>
2026-02-05 08:37:22 +0800

    Update fix gns3 fix runtime 25

--- gns3.nix
+++ gns3.nix
@@ -1,35 +1,29 @@
 { pkgs, ... }:
 
-{
-  # 1. Definisikan Grup secara Eksplisit
-  # Ini "memaksa" sistem membuat grup ubridge dan wireshark
-  users.groups.ubridge = {};
-  users.groups.wireshark = {};
-
-  # 2. Konfigurasi User
-  users.users.kaco = {
-    extraGroups = [ 
-      "ubridge"    # Untuk akses ubridge (kabel antar node)
-      "wireshark"  # Untuk sniffing paket
-      "libvirtd"   # Jika nanti pakai QEMU/KVM
-      "docker"     # Jika pakai node Docker
-    ];
+let
+  # Memaksa penggunaan Python 3.12 untuk GNS3
+  python312Packages = pkgs.python312Packages;
+  
+  gns3-gui-312 = pkgs.gns3-gui.override {
+    python3Packages = python312Packages;
   };
-
-  # 3. Paket Aplikasi Utama
-  environment.systemPackages = with pkgs; [
-    gns3-gui
-    gns3-server
-    ubridge
-    dynamips
-    vpcs
-    inetutils    # Menyediakan perintah 'telnet' untuk konsol
-    wireshark    # GUI untuk sniffing
-    xterm        # Terminal default GNS3
+  
+  gns3-server-312 = pkgs.gns3-server.override {
+    python3Packages = python312Packages;
+  };
+in
+{
+  users.users.kaco.packages = [
+    gns3-gui-312
+    gns3-server-312
+    pkgs.ubridge
+    pkgs.dynamips
+    pkgs.vpcs
+    pkgs.wireshark
+    pkgs.xterm
   ];
 
-  # 4. Security Wrappers (SANGAT PENTING)
-  # Memberikan izin khusus pada binary tanpa harus lari sebagai root
+  # Wrapper tetap sama seperti sebelumnya
   security.wrappers.ubridge = {
     source = "${pkgs.ubridge}/bin/ubridge";
     capabilities = "cap_net_admin,cap_net_raw+ep";
@@ -37,13 +31,6 @@
     group = "ubridge";
     permissions = "u+rx,g+x,o=";
   };
-
-  # 5. Konfigurasi Program Tambahan
-  programs.wireshark.enable = true;
-  programs.wireshark.package = pkgs.wireshark;
-
-  # 6. Izinkan IP Forwarding (Opsional - agar node bisa akses internet via host)
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-  };
+  
+  users.groups.ubridge = {};
 }
