649ef6ae48a85414f482174ba3c5a0ca52fefee8
hex4coder <the.programmer.luyo@gmail.com>
2026-03-08 04:29:54 +0800

    config: change plymouth theme to glitch

--- configuration.nix
+++ configuration.nix
@@ -41,7 +41,7 @@
   # Plymouth boot screen
   boot.plymouth = {
     enable = true;
-    theme = "circle";
+    theme = "glitch";
     themePackages = with pkgs; [
       adi1090x-plymouth-themes
     ];
