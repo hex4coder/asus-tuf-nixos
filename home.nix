ad27dc784813ab1548117e1c8902c0410f8e46e6
hex4coder <the.programmer.luyo@gmail.com>
2026-03-07 16:01:45 +0800

    feat: add n-up alias for full system maintenance

--- home.nix
+++ home.nix
@@ -68,6 +68,7 @@
       gsta = "git stash push";
       gstp = "git stash pop";
       ncb = "sudo nixos-rebuild switch --impure --flake . --upgrade";
+      n-up = "git pull && nix flake update && sudo nixos-rebuild switch --impure --flake . && git add flake.lock && git commit -m 'chore: system update' && git push";
     };
   };
 
