c5931c9df2d67c3464d5a7afd345d27c00c4eae6
hex4coder <the.programmer.luyo@gmail.com>
2026-03-08 04:38:44 +0800

    config: change plymouth theme to circle_hud

--- configuration.nix
+++ configuration.nix
@@ -41,7 +41,7 @@
   # Plymouth boot screen
   boot.plymouth = {
     enable = true;
-    theme = "glitch";
+    theme = "circle_hud";
     themePackages = with pkgs; [
       adi1090x-plymouth-themes
     ];
