52ecf2395bbb1ff2dd81aa4a6c2504dc4606b02c
hex4coder <the.programmer.luyo@gmail.com>
2026-03-14 09:18:43 +0800

    style: apply Pastel Powerline preset to Starship

--- configuration.nix
+++ configuration.nix
@@ -215,7 +215,65 @@
     enable = true;
     nix-direnv.enable = true;
   };
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = "[](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$c$elixir$elm$golang$gradle$haskell$java$julia$nodejs$nim$rust$scala[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)";
+      username = {
+        show_always = true;
+        style_user = "bg:#9A348E";
+        style_root = "bg:#9A348E";
+        format = "[$user ]($style)";
+        disabled = false;
+      };
+      os = {
+        style = "bg:#9A348E";
+        disabled = true;
+      };
+      directory = {
+        style = "bg:#DA627D";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+      directory.substitutions = {
+        "Documents" = "󰈙 ";
+        "Downloads" = " ";
+        "Music" = " ";
+        "Pictures" = " ";
+      };
+      git_branch = {
+        symbol = "";
+        style = "bg:#FCA17D";
+        format = "[ $symbol $branch ]($style)";
+      };
+      git_status = {
+        style = "bg:#FCA17D";
+        format = "[$all_status$ahead_behind ]($style)";
+      };
+      nodejs = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+      rust = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+      golang = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:#33658A";
+        format = "[ ♥ $time ]($style)";
+      };
+    };
+  };
 
   # untuk kdenlive
   nixpkgs.overlays = [
